4564727ddea59feacf060ccf33bb8d86b9cbf82f
Kimbali <kimgarcianton@hotmail.com>
2024-12-10 15:34:19 +0100

    handle cors error

--- server.js
+++ server.js
@@ -5,34 +5,44 @@ const yogaClassRoutes = require('./routes/yogaClassRoutes');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
 
-// Cargar variables de entorno
+// Load environment variables
 dotenv.config();
 
-// Conectar a MongoDB
+// Connect to MongoDB
 connectDB();
 
 const app = express();
 
-// CORS ///////
-// PROD: Configure CORS to allow requests from your Ionic app
-// app.use(cors({
-//   origin: 'http://localhost:3000', // Replace with your Ionic app's origin
-//   credentials: true // Allows cookies and credentials to be included
-// }));
+// CORS Configuration
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [
+  'http://localhost:8100',
+];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
-// DEV: Alternatively, for development purposes, you can allow all origins
-app.use(cors());
-
-// Middleware para parsear JSON
+// Middleware to parse JSON
 app.use(express.json());
 
-// Rutas
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/yoga-classes', yogaClassRoutes);
 
-// Puerto del servidor
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+});
+
+// Start the server
 const PORT = process.env.PORT || 5001;
-console.log('hey server');
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
